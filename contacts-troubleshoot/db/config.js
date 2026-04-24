8b0b47463984605e26518c074e125d6c77ae4f51
tulio-o <mtco.tulio@gmail.com>
2026-04-24 16:19:44 +0100

    fix: use better env var naming

--- contacts-troubleshoot/db/config.js
+++ contacts-troubleshoot/db/config.js
@@ -3,9 +3,9 @@ var util = require('util');
 
 // The following variables should be defined in the
 // secret resource associated with the database.
-var db_user = process.env["database-user"];
-var db_pass = process.env["database-password"];
-var db_name = process.env["database-name"];
+var db_user = process.env.DATABASE_USER
+var db_pass = process.env.DATABASE_PASSWORD
+var db_name = process.env.DATABASE_NAME;
 
 const DB_CONFIG = util.format('postgresql://%s:%s@%s:%s/%s', db_user, db_pass, process.env.POSTGRESQL_SERVICE_HOST, process.env.POSTGRESQL_SERVICE_PORT, db_name);
 
